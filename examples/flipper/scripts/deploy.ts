f89569bc8fdfdbe9c1e6c0e3abe26f2f0baf6158
opvqo <opvqo@outlook.com>
2020-10-29 01:24:15 +1300

    chore: fix example

---
+++ examples/flipper/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { patract } from "redspot/internal/lib/redspot-lib";
+
+const { getContractFactory, getSigners, getRandomSigner } = patract!;
+
+async function run() {
+  // const flipperFactory = await getContractFactory("flipper");
+  // await flipperFactory.deployed(0, true);
+  const signers = await getSigners();
+  await getRandomSigner(signers[0], "10000030921831283091283091283012");
+}
+
+run().catch((err) => {
+  console.log(err);
+});
+
+// process.on('unhandledRejection', (reason: any, p) => {
+//   console.log('Unhandled Rejection at: Promise', p, 'reason:')
+//   console.dir(reason.stack)
+//   // application specific logging, throwing an error, or other logic here
+// });
+
+// 5G1nKgFJSw8TdidwxXjJ8pqd32ztSQLPnAgQNwNoL9z1af2K
