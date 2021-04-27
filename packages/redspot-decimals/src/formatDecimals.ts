bfef9bfd3486bf96ffa759e4b6227f05795c4df4
Zhichao Zhang <zzcwoshizz@163.com>
2021-04-27 11:19:06 +0800

    refactor: formatDecimals func

--- packages/redspot-decimals/src/formatDecimals.ts
+++ packages/redspot-decimals/src/formatDecimals.ts
@@ -10,32 +10,25 @@ const units: string[] = [
 const regexp = new RegExp(`^(\\d+(\\.\\d+)*)\\s(${units.join('|')})$`, 'i');
 
 export function formatDecimals(value: AnyNumber, decimals: number): AnyNumber {
-  console.log(value, decimals);
   if (isString(value) && regexp.test(value)) {
     const [, num, , unit] = value.match(regexp);
 
-    if (unit.toUpperCase() === 'DOT') {
-      const [pre, suf] = num.split('.');
-      const preBn = new BN(pre || 0);
-      let sufBn: BN;
-      if (!suf) {
-        sufBn = new BN(0);
-      } else if (suf.length > decimals) {
-        sufBn = new BN(suf.slice(0, decimals));
-      } else {
-        sufBn = new BN(suf).mul(BN_TEN.pow(new BN(decimals - suf.length)));
-      }
-      console.log(
-        preBn
-          .mul(BN_TEN.pow(new BN(decimals)))
-          .add(sufBn)
-          .toString()
-      );
-      return preBn.mul(BN_TEN.pow(new BN(decimals))).add(sufBn);
-    } else if (unit.toUpperCase() === 'UNIT') {
-      const [pre] = num.split('.');
-      return pre;
+    if (unit.toUpperCase() === 'UNIT') {
+      decimals = 0;
     }
+
+    const [pre, suf] = num.split('.');
+    const preBn = new BN(pre || 0);
+    let sufBn: BN;
+    if (!suf) {
+      sufBn = new BN(0);
+    } else if (suf.length > decimals) {
+      sufBn = new BN(suf.slice(0, decimals));
+    } else {
+      sufBn = new BN(suf).mul(BN_TEN.pow(new BN(decimals - suf.length)));
+    }
+
+    return preBn.mul(BN_TEN.pow(new BN(decimals))).add(sufBn);
   }
 
   return value;
