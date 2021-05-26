d83bea9fe5bbcbfe15633f3e5d304b454f9f5882
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-05-26 15:39:22 +1200

    add europa types

--- packages/redspot-known-types/src/europa.ts
+++ packages/redspot-known-types/src/europa.ts
@@ -1,6 +1,30 @@
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
 const definitions: OverrideBundleDefinition = {
+  rpc: {
+    europa: {
+      forwardToHeight: {
+        description: '',
+        params: [
+          {
+            name: 'height',
+            type: 'u32'
+          }
+        ],
+        type: 'Bytes'
+      },
+      backwardToHeight: {
+        description: '',
+        params: [
+          {
+            name: 'height',
+            type: 'u32'
+          }
+        ],
+        type: 'Bytes'
+      }
+    }
+  },
   types: [
     {
       minmax: [0, undefined],
@@ -12,6 +36,7 @@ const definitions: OverrideBundleDefinition = {
           pairCount: 'u32',
           codeHash: 'CodeHash',
           rentAllowance: 'Balance',
+          rentPayed: 'Balance',
           deductBlock: 'BlockNumber',
           lastWrite: 'Option<BlockNumber>',
           _reserved: 'Option<Null>'
