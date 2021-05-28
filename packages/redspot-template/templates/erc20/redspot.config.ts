568c88c50d569bb8de8c7465e1f339701833d4a9
iiiiiiii <lllliliiii@outlook.com>
2021-05-28 19:27:31 +1200

    update template (#111)

--- packages/redspot-template/templates/erc20/redspot.config.ts
+++ packages/redspot-template/templates/erc20/redspot.config.ts
@@ -2,24 +2,28 @@ import { RedspotUserConfig } from 'redspot/types';
 import '@redspot/patract';
 import '@redspot/chai';
 import '@redspot/gas-reporter';
+import '@redspot/known-types';
+import '@redspot/watcher';
+import '@redspot/explorer';
+import '@redspot/decimals';
 
 export default {
   defaultNetwork: 'development',
   contract: {
     ink: {
+      docker: true,
       toolchain: 'nightly',
-      docker: false,
       sources: ['contracts/**/*']
     }
   },
   networks: {
     development: {
       endpoint: 'ws://127.0.0.1:9944',
-      types: {},
-      gasLimit: '400000000000'
+      gasLimit: '400000000000',
+      types: {}
     },
-    substrate: {
-      endpoint: 'ws://127.0.0.1:9944',
+    jupiter: {
+      endpoint: 'wss://jupiter-poa.elara.patract.io',
       gasLimit: '400000000000',
       accounts: ['//Alice'],
       types: {}
@@ -27,5 +31,10 @@ export default {
   },
   mocha: {
     timeout: 60000
+  },
+  docker: {
+    sudo: false,
+    runTestnet:
+      "docker run -p 9944:9944 --rm redspot/contract /bin/bash -c 'canvas --rpc-cors all --tmp --dev --ws-port=9944 --ws-external'"
   }
 } as RedspotUserConfig;
