a09299f2a1300df8bca666dd7dde0a9dfca13f9c
iiiiiiiii <69485494+ii-ii-ii@users.noreply.github.com>
2021-05-28 19:26:20 +1200

    update template

--- examples/erc20/redspot.config.ts
+++ examples/erc20/redspot.config.ts
@@ -1,32 +1,40 @@
-import { RedspotUserConfig } from 'redspot/types';
-import '@redspot/patract';
-import '@redspot/chai';
-import '@redspot/gas-reporter';
-import '@redspot/known-types';
+import { RedspotUserConfig } from "redspot/types";
+import "@redspot/patract";
+import "@redspot/chai";
+import "@redspot/gas-reporter";
+import "@redspot/known-types";
+import "@redspot/watcher";
+import "@redspot/explorer";
+import "@redspot/decimals";
 
 export default {
-  defaultNetwork: 'development',
+  defaultNetwork: "development",
   contract: {
     ink: {
-      docker: false,
-      toolchain: 'nightly',
-      sources: ['contracts/**']
-    }
+      docker: true,
+      toolchain: "nightly",
+      sources: ["contracts/**/*"],
+    },
   },
   networks: {
     development: {
-      endpoint: 'ws://127.0.0.1:9944',
+      endpoint: "ws://127.0.0.1:9944",
+      gasLimit: "400000000000",
       types: {},
-      gasLimit: '400000000000'
     },
-    substrate: {
-      endpoint: 'ws://127.0.0.1:9944',
-      gasLimit: '400000000000',
-      accounts: ['//Alice'],
-      types: {}
-    }
+    jupiter: {
+      endpoint: "wss://jupiter-poa.elara.patract.io",
+      gasLimit: "400000000000",
+      accounts: ["//Alice"],
+      types: {},
+    },
   },
   mocha: {
-    timeout: 60000
-  }
+    timeout: 60000,
+  },
+  docker: {
+    sudo: false,
+    runTestnet:
+      "docker run -p 9944:9944 --rm redspot/contract /bin/bash -c 'canvas --rpc-cors all --tmp --dev --ws-port=9944 --ws-external'",
+  },
 } as RedspotUserConfig;
--- examples/erc20/tests/erc20.test.ts
+++ examples/erc20/tests/erc20.test.ts
@@ -1,10 +1,9 @@
-import BN from "bn.js";
 import { expect } from "chai";
-import { patract, network, artifacts } from "redspot";
+import { artifacts, network, patract } from "redspot";
 
 const { getContractFactory, getRandomSigner } = patract;
 
-const { api, getSigners } = network;
+const { api, getAddresses, getSigners } = network;
 
 describe("ERC20", () => {
   after(() => {
@@ -12,62 +11,60 @@ describe("ERC20", () => {
   });
 
   async function setup() {
-    const one = new BN(10).pow(new BN(api.registry.chainDecimals[0]));
-    const signers = await getSigners();
-    const Alice = signers[0];
-    const { address: sender } = await getRandomSigner(Alice, one.muln(1000));
-
-    const contractFactory = await getContractFactory("erc20", sender);
+    await api.isReady
+    const signerAddresses = await getAddresses();
+    const Alice = signerAddresses[0];
+    const sender = await getRandomSigner(Alice, "10000 UNIT");
+    const contractFactory = await getContractFactory("erc20", sender.address);
     const contract = await contractFactory.deploy("new", "1000");
     const abi = artifacts.readArtifact("erc20");
-    const { address: receiver } = await getRandomSigner();
+    const receiver = await getRandomSigner();
 
-    return { sender, contractFactory, contract, abi, receiver, Alice, one };
+    return { sender, contractFactory, contract, abi, receiver, Alice };
   }
 
   it("Assigns initial balance", async () => {
     const { contract, sender } = await setup();
-    const result = await contract.query.balanceOf(sender);
-    expect(result.output).to.eq(1000);
+    const result = await contract.query.balanceOf(sender.address);
+    expect(result.output).to.equal(1000);
   });
 
   it("Transfer adds amount to destination account", async () => {
     const { contract, receiver } = await setup();
-    await expect(() => contract.tx.transfer(receiver, 7)).to.changeTokenBalance(
-      contract,
-      receiver,
-      7
-    );
 
     await expect(() =>
-      contract.tx.transfer(receiver, 7)
+      contract.tx.transfer(receiver.address, 7)
+    ).to.changeTokenBalance(contract, receiver, 7);
+
+    await expect(() =>
+      contract.tx.transfer(receiver.address, 7)
     ).to.changeTokenBalances(contract, [contract.signer, receiver], [-7, 7]);
   });
 
   it("Transfer emits event", async () => {
     const { contract, sender, receiver } = await setup();
 
-    await expect(contract.tx.transfer(receiver, 7))
+    await expect(contract.tx.transfer(receiver.address, 7))
       .to.emit(contract, "Transfer")
-      .withArgs(sender, receiver, 7);
+      .withArgs(sender.address, receiver.address, 7);
   });
 
   it("Can not transfer above the amount", async () => {
     const { contract, receiver } = await setup();
 
-    await expect(contract.tx.transfer(receiver, 1007)).to.not.emit(
+    await expect(contract.tx.transfer(receiver.address, 1007)).to.not.emit(
       contract,
       "Transfer"
     );
   });
 
   it("Can not transfer from empty account", async () => {
-    const { contract, Alice, one, sender } = await setup();
+    const { contract, Alice, sender } = await setup();
 
-    const emptyAccount = await getRandomSigner(Alice, one.muln(10000));
+    const emptyAccount = await getRandomSigner(Alice, "10 UNIT");
 
     await expect(
-      contract.connect(emptyAccount).tx.transfer(sender, 7)
+      contract.connect(emptyAccount).tx.transfer(sender.address, 7)
     ).to.not.emit(contract, "Transfer");
   });
 });
