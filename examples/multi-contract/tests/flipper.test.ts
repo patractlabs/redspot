3e508151c326c960997d9efb70dc37713d9f8fa7
ii-ii-ii <69485494+ii-ii-ii@users.noreply.github.com>
2021-06-02 12:21:13 +1200

    update fliper test

--- examples/multi-contract/tests/flipper.test.ts
+++ examples/multi-contract/tests/flipper.test.ts
@@ -1,40 +1,45 @@
-import { patract, network } from 'redspot';
+import { expect } from 'chai';
+import { artifacts, network, patract } from 'redspot';
 
-const { getContractFactory } = patract;
-const { createSigner, keyring, api } = network;
+const { getContractFactory, getRandomSigner } = patract;
 
-async function run() {
+const { api, getAddresses, getSigners } = network;
+
+async function setup() {
   await api.isReady;
+  const signerAddresses = await getAddresses();
+  const Alice = signerAddresses[0];
+  const contractFactory = await getContractFactory('flipper', Alice);
 
-  // The redspot signer supports passing in an address. If you want to use  substrate uri, you can do it like this:
-  // const signer = createSigner(keyring.createFromUri("bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice"));
-  // Or get the configured account from redspot config:
-  // const signer = (await getSigners())[0]
-  const signer = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'; // Alice Address
-
-  const contractFactory = await getContractFactory('flipper', signer);
-
-  const balance = await api.query.system.account(signer);
-
-  console.log('Balance: ', balance.toHuman());
-
-  // The `deploy` method will attempt to deploy a new contract.
-  // The `deployed` method will first find out if the same contract already exists based on the parameters.
-  // If the contract exists, it will be returned, otherwise a new contract will be created.
-  const contract = await contractFactory.deploy('new', false, {
-    gasLimit: '400000000000',
-    value: '1000 UNIT'
-  });
-
-  console.log('');
-  console.log(
-    'Deploy successfully. The contract address: ',
-    contract.address.toString()
-  );
-
-  api.disconnect();
+  return { contractFactory, Alice };
 }
 
-run().catch((err) => {
-  console.log(err);
+describe('Flipper', () => {
+  after(() => {
+    return api.disconnect();
+  });
+
+  it('deploy flipper with initValue', async () => {
+    const { contractFactory } = await setup();
+    const contract = await contractFactory.deploy('new', true);
+
+    const result = await contract.query.get();
+
+    expect(result.output).to.equal(true);
+  });
+
+  it('flipper it', async () => {
+    const { contractFactory } = await setup();
+    const contract = await contractFactory.deploy('new', false);
+
+    const beforeResult = await contract.query.get();
+
+    expect(beforeResult.output).to.equal(false);
+
+    await contract.tx.flip();
+
+    const afterFlipResult = await contract.query.get();
+
+    expect(afterFlipResult.output).to.equal(true);
+  });
 });
